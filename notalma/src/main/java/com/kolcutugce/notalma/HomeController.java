6857937d70d5995477fd6a1b398f397ad2c081b7
tugcekolcu <tugcekolcu@users.noreply.github.com>
2019-02-16 22:00:47 +0300

    Hello World
    initial commit for project settings

---
+++ notalma/src/main/java/com/kolcutugce/notalma/HomeController.java
@@ -0,0 +1,42 @@
+package com.kolcutugce.notalma;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/error_404", method = RequestMethod.GET)
+	public String error( Model model) {
+
+		return "error_404";
+	}
+	
+	
+}
---
+++ notalma/src/main/webapp/WEB-INF/views/error_404.jsp
@@ -0,0 +1,15 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Hata</title>
+</head>
+<body>
+<h1>
+	Page not found
+</h1>
+
+</body>
+</html>
